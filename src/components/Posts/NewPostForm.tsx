0e0575d6f449bfab7a3693a4d121cb10d61e72e0
Abdulbasit <habdulbasit099@gmail.com>
2023-05-23 12:40:13 +0200

    built tab component

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,8 +1,59 @@
-import React from "react";
-
+import { Flex, Icon } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { BiPoll } from "react-icons/bi";
+import { BsLink45Deg, BsMic } from "react-icons/bs";
+import { IoDocumentText, IoImageOutline } from "react-icons/io5";
+import { AiFillCloseCircle } from "react-icons/ai";
+import Tabitem from "./Tabitem";
 type NewPostFormProps = {};
 
+const formTabs = [
+  {
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    title: "Image & Video",
+    icon: IoImageOutline,
+  },
+
+  {
+    title: "Link",
+    icon: BsLink45Deg,
+  },
+  {
+    title: "Poll",
+    icon: BiPoll,
+  },
+  {
+    title: "Talk",
+    icon: BsMic,
+  },
+];
+
+export type tabItem = {
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
 const NewPostForm: React.FC<NewPostFormProps> = () => {
-  return <div>Have a good coding</div>;
+  const [selectedTabs, setSelectedTabs] = useState(formTabs[0].title);
+  return (
+    <Flex direction="column" bg="white" borderRadius="4" mt="2">
+      <Flex width="100%">
+        {formTabs.map((item) => {
+          return (
+            <>
+              <Tabitem
+                item={item}
+                selected={item.title === selectedTabs}
+                setSelectedTab={setSelectedTabs}
+              />
+            </>
+          );
+        })}
+      </Flex>
+    </Flex>
+  );
 };
 export default NewPostForm;
